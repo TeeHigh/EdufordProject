86aca6c8bf529a0a97a77700f17724533ba9e479
TeeHigh <105116838+TeeHigh@users.noreply.github.com>
2022-08-30 14:15:12 +0100

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@yourwebsite.com';
+$email_from = 'info@https://teehigh.github.io/EdufordProject.com';
 
 $email_subject = 'New Form Submission';
 
@@ -13,7 +13,7 @@ $email_body = "User name: $name.\n".
                 "Subject: $subject.\n".
                 "User message: $message.\n";
 
-$to = 'ifeoluwaset@gmail.com';
+$to = 'taiwoifeoluwa023@gmail.com';
 
 $headers = "From: $email_from \r\n";
 
